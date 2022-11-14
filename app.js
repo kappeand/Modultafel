003f4d7c7b044c2e467329c0222d749d7acdaccf
Laurent Mouchart <laurent@mouchart.ch>
2022-11-14 15:41:01 +0100

    Reading of excel File & mapping data in Backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const xlsx = require('xlsx');
 
 
 app.use(express.static('public'), fileUpload({
@@ -23,6 +24,15 @@ app.post('/upload', function (req, res) {
     excelFile.mv(uploadPath, function (err) {
         if (err)
             return res.status(500).send(err);
+
+        const workbook = xlsx.readFile(uploadPath); // parse excel file
+        const sheetNames = workbook.SheetNames;
+        const module = xlsx.utils.sheet_to_json(workbook.Sheets[sheetNames[1]]);
+
+        module.map(modul => {
+            console.log(`${modul.Modulbezeichnung} hat ${modul.ECTS} ECTS `);
+            //TODO @Andrin generate HTML from modules
+        })
 
         //TODO @Andrin return generated file here instead of Modultafel.html
         res.attachment("Modultafel.html");
