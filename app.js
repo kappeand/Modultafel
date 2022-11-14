3ef075f6a800f48f9957edcb973b9dafa56aed2b
Laurent Mouchart <laurent@mouchart.ch>
2022-11-14 14:47:49 +0100

    File upload working

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
-const path = require("path");
-
-const PORT = process.env.PORT || 8000;
-
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'));
 
-app.post('/upload', (
-    fileUpload({createParentPath: true}),
-    (req, res) => {
-        const file = req.files;
-        console.log(file)
-        return res.json({status:'logged', message:'logged'})
+app.use(express.static('public'), fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}));
+
+app.post('/upload', function (req, res) {
+    let sampleFile;
+    let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
     }
-));
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + '/tmp/' + Date.now() + "_" + sampleFile.name;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function (err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+});
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
 
--- public/Script.js
+++ public/Script.js
@@ -2,9 +2,9 @@ const form = document.getElementById('fileUploadForm');
 const sendFiles = async () => {
     const file = document.getElementById('formFile').files[0];
     const formData = new FormData();
-    formData.append(file.name, file);
+    formData.append("fileupload", file);
     console.log(formData)
-    const response = await fetch('http://localhost:8000/upload', {method: 'POST', body: formData});
+    const response = await fetch('http://localhost:8080/upload', {method: 'POST', body: formData});
     const json = await response.json();
     console.log(json);
 }
