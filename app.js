bf1eb4677a1e419ad23b0d2b4b978e0b8ff369bb
Laurent Mouchart <laurent@mouchart.ch>
2022-11-17 16:28:07 +0100

    Updated style of genereated Modultafel. Printing with color now working by removing @Media print tags in style.ejs. Added new Text fields to Excel template

--- app.js
+++ app.js
@@ -32,12 +32,10 @@ app.post('/upload', function (req, res) {
 
         const modules = xlsx.utils.sheet_to_json(workbook.Sheets[sheetNames[1]]);
         const colors = xlsx.utils.sheet_to_json(workbook.Sheets[sheetNames[0]]);
-        console.log(colors);
 
         modules.map(module => {
             module.FarbeModulkaestchen = colors.find(color => color.Modulgruppe == module.Modulgruppe).FarbeModulkaestchen;
             module.Hintergrundfarbe = colors.find(color => color.Modulgruppe == module.Modulgruppe).Hintergrundfarbe;
-            console.log(module);
         });
 
         let semesterArray = [];
